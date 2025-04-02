fb1a21dc6f80a05b64204749eab5b1b7b6861373
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2025-04-02 20:42:47 +0600

    updated
    updated

---
+++ main.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgb(89 46 131)',
+          secondary: 'rgba(89, 46, 131, 0.6)',
+
+        }
+      }
+    }
+  }
